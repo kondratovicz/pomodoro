581b9ea9e235020a495566b84229bc44bf814c06
Pavel <kondratovich@ro.ru>
2023-05-06 16:18:16 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+window = tkinter.Tk()
+window.title("Pomodoro!")
+window.config(padx=100, pady=50)
+canvas = tkinter.Canvas(width=200, height= 224)
+pomodoro_image = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=pomodoro_image)
+canvas.create_text(text="allo")
+canvas.pack()
+
+
+
+window.mainloop()
